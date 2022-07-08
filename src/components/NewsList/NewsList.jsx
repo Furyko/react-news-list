f2564fe49ecd48c74d2a14940eb91947a62767ff
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-07-08 18:45:40 -0300

    Changed the display of the cards class to grid. Refactored fetch in the NewsList component

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -6,7 +6,7 @@ function NewsList() {
     const apiKey = 'd751ad452b2646ca96892d86f2931b9a'
     const [articles, setArticles] = useState([])
     useEffect(() => {
-        fetch('https://newsapi.org/v2/everything?apiKey=' + apiKey + '&q=bitcoin&pageSize=10')
+        fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=bitcoin&pageSize=10`)
         .then(res => res.json())
         .then(data => setArticles(data.articles))
     }, [])
