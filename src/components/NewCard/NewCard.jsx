2abf96ff8cba73963ed89bf65deea200a535a21b
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-07-08 17:10:58 -0300

    Added card component for news with its styles

---
+++ src/components/NewCard/NewCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NewCard.css';
+
+function NewCard({ article }) {
+    return (
+        <div className='card'>
+            <a href={ article.url }>
+                <div className='card-info'>
+                    <h3>{ article.title }</h3>
+                    <p>{ article.description }</p>
+                    <p>{ article.publishedAt }</p>
+                </div>
+                <div className='card-image'>
+                    <img src={ article.urlToImage } alt='No es posible cargar la imagen.'/>
+                </div>
+            </a>
+        </div>
+    )
+}
+
+export default NewCard;
