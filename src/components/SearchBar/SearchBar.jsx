350903b69f52c2b8f7cd466955eb138ce791ebe7
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-07-09 18:30:19 -0300

    Added length validation to the search input in SearchBar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,11 +6,24 @@ function SearchBar() {
         e.preventDefault();
         alert(e.target.search.value);
     }
+    function onChange(e) {
+        const buttonElement = document.getElementById('search-button')
+        if (e.target.value.length > 2) {
+            buttonElement.disabled = false
+        } else {
+            buttonElement.disabled = true
+        }
+    }
     return (
         <div className='search-bar-container'>
             <form onSubmit={onSubmit}>
-                <input type='text' name='search' placeholder='Introduzca una busqueda (ej: bitcoin)' className='search-input'/>
-                <input type='submit' value='Buscar' className='search-button'/>
+                <input 
+                type='text' 
+                name='search' 
+                placeholder='Introduzca una busqueda (ej: bitcoin)' 
+                className='search-input'
+                onChange={onChange}/>
+                <input type='submit' value='Buscar' className='search-button' id='search-button' disabled/>
             </form>
         </div>
     )
