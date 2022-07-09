600f6a71a2b3d7c3147b45ab24f9ff108221db15
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-07-09 18:09:32 -0300

    Added total results feedback in NewsList component

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -7,13 +7,16 @@ function NewsList() {
     const apiKey = 'd751ad452b2646ca96892d86f2931b9a'
     const [articles, setArticles] = useState([])
     useEffect(() => {
-        fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=bitcoin&pageSize=10`)
+        fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=bitcoin&pageSize=10&language=es`)
         .then(res => res.json())
-        .then(data => setArticles(data.articles))
+        .then(data => setArticles(data))
     }, [])
     return (
         <div className='cards-container'>
-            {articles.map((item, index) => (
+            {articles.totalResults &&
+            <span className='results-total'>Está viendo 10 noticias de {articles.totalResults} resultados</span>
+            }
+            {articles.articles && articles.articles.map((item, index) => (
                 <NewCard article={item} key={index}/>
             ))}
         </div>
