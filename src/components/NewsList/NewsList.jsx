988058458f680cf84ab09161823576dcf9b9e5bc
Julian Palavecino <samuguayfenix@gmail.com>
2022-07-11 21:40:41 -0300

    Implemented SearchContext in NewsList.jsx.

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import NewCard from '../NewCard/NewCard';
 import './NewsList.css';
+import { SearchContext } from '../../contexts/SearchContext';
 
 function NewsList() {
+    const state = useContext(SearchContext)
     const apiKey = 'd751ad452b2646ca96892d86f2931b9a'
     const [articles, setArticles] = useState([])
     useEffect(() => {
-        fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=bitcoin&pageSize=10&language=es`)
-        .then(res => res.json())
-        .then(data => setArticles(data))
-    }, [])
+        if (state.search) {
+            fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=${state.search}&pageSize=10&language=es`)
+            .then(res => res.json())
+            .then(data => setArticles(data))
+        }
+    }, [state])
     return (
         <div className='cards-container'>
             {articles.totalResults &&
