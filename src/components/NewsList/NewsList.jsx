8620ec1fec620029bb168289bf8cfcd12f8d155b
Julian Palavecino <samuguayfenix@gmail.com>
2022-07-11 22:29:07 -0300

    Added loading feedback while fetch to api is pending

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -8,19 +8,28 @@ function NewsList() {
     const state = useContext(SearchContext)
     const apiKey = 'd751ad452b2646ca96892d86f2931b9a'
     const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         if (state.search) {
+            setLoading(true)
             fetch(`https://newsapi.org/v2/everything?apiKey=${apiKey}&q=${state.search}&pageSize=10&language=es`)
             .then(res => res.json())
-            .then(data => setArticles(data))
+            .then(data => {
+                setArticles(data);
+                setLoading(false)
+            })
+            .catch(err => {
+                console.error(err)
+                setLoading(false)
+            })
         }
     }, [state])
     return (
         <div className='cards-container'>
-            {articles.totalResults &&
+            { articles.totalResults &&
             <span className='results-total'>Está viendo 10 noticias de {articles.totalResults} resultados</span>
             }
-            {articles.articles && articles.articles.map((item, index) => (
+            { loading ? <div>cargando...</div> : articles.articles && articles.articles.map((item, index) => (
                 <NewCard article={item} key={index}/>
             ))}
         </div>
