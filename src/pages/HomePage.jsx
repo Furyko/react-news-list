de86c13339b72a8c0e8e28f37708add5f6e48bf5
Julian Palavecino <samuguayfenix@gmail.com>
2022-07-16 17:26:03 -0300

    Added PageContext

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,16 +3,20 @@ import Navbar from '../components/Navbar/Navbar';
 import NewsList from '../components/NewsList/NewsList';
 import SearchBar from '../components/SearchBar/SearchBar';
 import { SearchContext } from '../contexts/SearchContext';
+import { PageContext } from '../contexts/PageContext';
 
 function HomePage() {
     const [search, setSearch] = useState('');
+    const [page, setPage] = useState(1);
     return (
         <div>
             <Navbar/>
             <main>
                 <SearchContext.Provider value={{search: search, setSearch: setSearch}}>
-                    <SearchBar/>
-                    <NewsList/>
+                    <PageContext.Provider value={{page: page, setPage: setPage}}>
+                        <SearchBar/>
+                        <NewsList/>
+                    </PageContext.Provider>
                 </SearchContext.Provider>
             </main>
         </div>
